738a035e7c448aaa634657d677ecd3b180725217
Vitaly <kuraev_vf@mail.ru>
2021-10-06 12:13:51 +0500

    example

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.example.test
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var tv: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv = findViewById(R.id.tv)
+
+        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        registerReceiver(batteryBroadcastReceiver, intentFilter)
+
+
+    }
+    private val batteryBroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            if(p1?.action == "android.intent.action.BATTERY_CHANGED") {
+                val level = p1.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+                tv.text = level.toString().plus(" ").plus("%")
+            }
+        }
 
     }
 
